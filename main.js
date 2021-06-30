0db1b542b3afd93b42e1a75530a5975f0f75cc0a
Jeremiah-Ng <72901442+Jeremiah-Ng@users.noreply.github.com>
2021-06-30 15:57:42 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function same(){
+    document.getElementById("heading1").style.backgroundColor="#f0ad4e";
+}
+function same1(){
+    document.getElementById("speed").style.backgroundColor="#d9534f";
+}
